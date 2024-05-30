942e2a4447596f51de7842b7e39155058c29d9af
Bryan De Houwer <bryandehouwer@hotmail.com>
2024-05-30 17:17:09 +0200

    fix ssl bug

--- LdapRelayScan.py
+++ LdapRelayScan.py
@@ -120,27 +120,29 @@ def InternalDomainFromAnonymousLdap(nameserverIp):
 #no error at all. Any other "successful" edge cases
 #not yet accounted for.
 def DoesLdapsCompleteHandshake(dcIp):
-  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  s.settimeout(5)
-  ssl_sock = ssl.wrap_socket(s,
-                            cert_reqs=ssl.CERT_OPTIONAL,
-                            suppress_ragged_eofs=False,
-                            do_handshake_on_connect=False)
-  ssl_sock.connect((dcIp, 636))
-  try:
-    ssl_sock.do_handshake()
-    ssl_sock.close()
-    return True
-  except Exception as e:
-    if "CERTIFICATE_VERIFY_FAILED" in str(e):
-        ssl_sock.close()
-        return True
-    if "handshake operation timed out" in str(e):
-        ssl_sock.close()
-        return False
-    else:
-      print("Unexpected error during LDAPS handshake: " + str(e))
-    ssl_sock.close()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(5)
+    context = ssl.create_default_context()
+    context.verify_mode = ssl.CERT_OPTIONAL
+    context.check_hostname = False
+    ssl_sock = context.wrap_socket(s,
+                              suppress_ragged_eofs=False,
+                              do_handshake_on_connect=False)
+    ssl_sock.connect((dcIp, 636))
+    try:
+      ssl_sock.do_handshake()
+      ssl_sock.close()
+      return True
+    except Exception as e:
+      if "CERTIFICATE_VERIFY_FAILED" in str(e):
+          ssl_sock.close()
+          return True
+      if "handshake operation timed out" in str(e):
+          ssl_sock.close()
+          return False
+      else:
+        print("Unexpected error during LDAPS handshake: " + str(e))
+      ssl_sock.close()
 
 
 #Conduct and LDAP bind and determine if server signing
